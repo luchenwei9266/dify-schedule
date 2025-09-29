eb4fc13f74bf6094d6c43a88df720b494ede850e
lu <lugege9266@gmail.com>
2025-09-29 09:31:11 +0800

    Update ql_dify_workflow.js

--- ql_dify_workflow.js
+++ ql_dify_workflow.js
@@ -70,6 +70,7 @@ class WorkflowTask extends Task {
     console.log(`Dify工作流【${info.data.name}】开始执行...`);
     const response = await workflow.getWorkflowResult(inputs, user, true);
     this.result = response.text || "";
+    console.log(this.result);
   }
 
   toString() {
